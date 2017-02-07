db448ab0ef9e2d90186c72fed23bf0360b71ce01
Dmitry Kolinchuk <dmko@sitecore.net>
2017-02-07 11:41:20 +0200

    Consider the context language of an item when using the search command in Experience Editor

---
+++ src/Sitecore.Support.95479/GetItemUrlRequest.cs
@@ -0,0 +1,49 @@
+﻿using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.ExperienceEditor.Speak.Server.Contexts;
+using Sitecore.ExperienceEditor.Speak.Server.Requests;
+using Sitecore.ExperienceEditor.Speak.Server.Responses;
+using Sitecore.Globalization;
+using Sitecore.Text;
+using Sitecore.Web;
+
+namespace Sitecore.Support.ExperienceEditor.Speak.Ribbon.Requests.Search
+{
+    public class GetItemUrlRequest : PipelineProcessorRequest<ValueItemContext>
+    {
+        public override PipelineProcessorResponseValue ProcessRequest()
+        {
+            string value = base.RequestContext.Value;
+            Assert.IsNotNull(value, "Could not get item ID for requestArgs:{0}", new object[]
+            {
+                base.Args.Data
+            });
+            Item item = base.RequestContext.Item;
+            Assert.IsNotNull(item, "Could not get Item for requestArgs:{0}", new object[]
+            {
+                base.Args.Data
+            });
+            string path = ID.IsID(value) ? value : Sitecore.Shell.Applications.WebEdit.Commands.Search.ParseForAttribute(value, "id");
+            Language language;
+            Item item2;
+            if (Language.TryParse(base.RequestContext.Language, out language))
+            {
+                item2 = item.Database.GetItem(path, language);
+            }
+            else
+            {
+                item2 = item.Database.GetItem(path);
+            }
+            if (item2 != null)
+            {
+                string itemUrl = WebEditUtil.GetItemUrl(item2);
+                return new PipelineProcessorResponseValue
+                {
+                    Value = new UrlString(itemUrl).GetUrl()
+                };
+            }
+            return new PipelineProcessorResponseValue();
+        }
+    }
+}
---
+++ src/Sitecore.Support.95479/sitecore/shell/client/Sitecore/ExperienceEditor/Pipelines/Search/SearchForItem.OpenDialog.js
@@ -0,0 +1,13 @@
+﻿define(["sitecore", "/-/speak/v1/ExperienceEditor/ExperienceEditor.js"], function (Sitecore, ExperienceEditor) {
+    return ExperienceEditor.PipelinesUtil.generateDialogCallProcessor({
+        url: function (context) { return context.currentContext.value; },
+        features: "dialogHeight: 700px;dialogWidth: 1300px;",
+        onSuccess: function (context, itemId) {
+            // Sitecore.Support.95479
+			// context.currentContext.value = itemId;
+            context.currentContext.value = itemId.split('/')[0];
+            context.currentContext.argument = context.currentContext.language;
+            context.currentContext.language = itemId.split('/').pop();
+        }
+    });
+});
